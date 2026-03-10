2ec56de1becbb3f331a26e30bec43bd67b9b6f96
Anandam Chattrapate <23eg105e05@anurag.edu.in>
2026-03-10 21:50:54 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,19 +2,12 @@ import { NavLink } from "react-router";
 import Search from "./Search";
 
 const Header = () => {
-  
-
   return (
     <header className="bg-white shadow-md">
       <div className="max-w-6xl mx-auto flex justify-between items-center p-4">
-        
-        {/* Logo */}
         <h1 className="">
           prod-demo
         </h1>
-        
-        
-        {/* Navigation */}
         <nav>
           <ul className="flex gap-4 items-center">
             <li>
@@ -22,13 +15,11 @@ const Header = () => {
                 Home
               </NavLink>
             </li>
-
             <li>
               <NavLink to="/contacts" className={({isActive})=>isActive?"text-white bg-blue-600 px-4 py-2 rounded-md":""}>
                 Contacts
               </NavLink>
             </li>
-
             <li>
               <NavLink to="/products" className={({isActive})=>isActive?"text-white bg-blue-600 px-4 py-2 rounded-md":""}>
                 Products
@@ -43,31 +34,3 @@ const Header = () => {
 };
 
 export default Header;
-
-
-
-
-
-
-
-// import { NavLink } from "react-router";
-
-// const Header = () => {
-//     return (
-//         <div>
-//             <ul>
-//                 <li>
-//                     <NavLink to="/">Home</NavLink>
-//                 </li>
-//                 <li>
-//                     <NavLink to="/contacts">Contacts</NavLink>
-//                 </li>
-//                 <li>
-//                     <NavLink to="/products">Products</NavLink>
-//                 </li>
-//             </ul>
-//         </div>
-//     );
-// };
-
-// export default Header;
